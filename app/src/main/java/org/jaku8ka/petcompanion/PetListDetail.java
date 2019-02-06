8d2399ef933083d8f008295f5882f931876b7281
jaku8ka <jakub.blahunka@gmail.com>
2019-02-06 19:36:45 +0100

    anims

---
+++ app/src/main/java/org/jaku8ka/petcompanion/PetListDetail.java
@@ -0,0 +1,29 @@
+package org.jaku8ka.petcompanion;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PetListDetail extends Fragment {
+
+
+    public PetListDetail() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pet_list_detail, container, false);
+    }
+
+}
